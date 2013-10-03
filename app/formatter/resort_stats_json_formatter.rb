e7234e38b7db4dcb0d5ab82e4a603dd7b55ea0ce
Emilio Gutter <egutter@10pines.com>
2013-10-03 10:10:15 -0300

    Changed date format

--- app/formatter/resort_stats_json_formatter.rb
+++ app/formatter/resort_stats_json_formatter.rb
@@ -7,7 +7,7 @@ class ResortStatsJsonFormatter
   def to_json
     json_result = {:reservation_date => [], :occupancy => [], :rate => [], :revPar => []}
     @resort_stats_collection.each_day_stats { |resort_stats|
-      json_result[:reservation_date] << resort_stats.reservation_date.strftime('%d/%m')
+      json_result[:reservation_date] << resort_stats.reservation_date.strftime('%d %b')
       json_result[:occupancy] << resort_stats.occupancy_percentage
       json_result[:rate] << resort_stats.rate_average
       json_result[:revPar] << resort_stats.revenue_per_available_room
