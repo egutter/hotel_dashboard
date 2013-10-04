072412e98fab61da737d4cd569ea6394ff29ff1a
Emilio Gutter <egutter@10pines.com>
2013-10-04 12:58:21 -0300

    Moved today index a little to avoid it being hidden

--- app/formatter/resort_stats_json_formatter.rb
+++ app/formatter/resort_stats_json_formatter.rb
@@ -5,7 +5,7 @@ class ResortStatsJsonFormatter
   end
 
   def to_json
-    json_result = {:reservation_date => [], :occupancy => [], :rate => [], :revPar => [], :today_index => @resort_stats_collection.index_at(Date.today)}
+    json_result = {:reservation_date => [], :occupancy => [], :rate => [], :revPar => [], :today_index => index_at_today_for_reservation_date()}
     @resort_stats_collection.each_day_stats { |resort_stats|
       json_result[:reservation_date] << resort_stats.reservation_date.strftime('%d %b')
       json_result[:occupancy] << resort_stats.occupancy_percentage
@@ -13,5 +13,10 @@ class ResortStatsJsonFormatter
       json_result[:revPar] << resort_stats.revenue_per_available_room
     }
     json_result
+  end
+
+  def index_at_today_for_reservation_date
+    index = @resort_stats_collection.index_at(Date.today)
+    return index + 0.5 unless index.nil?
   end
 end
